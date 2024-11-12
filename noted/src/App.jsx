989c5a02d2b21cf9c969ce476e87ac096ae70aef
Alex Rhea <ajrhea03@gmail.com>
2024-11-11 23:15:37 -0600

    CRUD and Misc.

--- noted/src/App.jsx
+++ noted/src/App.jsx
@@ -2,13 +2,26 @@
 
 import React from 'react';
 import AccountList from './AccountList';
+import CRUD from 'noted/server/CRUD';
+
 function App() {
+  CRUD.add()
+  const addItem = () => {
+
+  }
   return (
     <div>
       <h1>Hello, Vite + React!</h1>
       <AccountList/>
+      <body>
+      <input type="text" id="NU" placeholder="New Username"></input>
+      <input type="text" id="NP" placeholder="New Password"></input>
+      <input type="text" id="NPT" placeholder="New Project Total"></input>
+      <button onclick={addItem}></button>
+      </body>
     </div>
   );
 }
+
 
 export default App;
